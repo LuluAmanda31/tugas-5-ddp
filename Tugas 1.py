41f4b425487efbc3d3e7246cdd5df15f5190cd5f
LuluAmanda31 <lulua3714@gmail.com>
2025-10-23 21:39:21 +0700

    Add files via upload

---
+++ Tugas 1.py
@@ -0,0 +1,4 @@
+kendaraan = ["nama kendaraan","jenis kendaraan","cc kendaraan","warna kendaraan","roda kendaraan"]
+kendaraan.extend(['harga kendaraan',"tipe kendaraan"])
+kendaraan.insert(2,"merk kendaraan")
+print(kendaraan)
